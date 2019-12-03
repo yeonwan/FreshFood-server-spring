a840e8e04331327e4ddc962c9554ebc495304608
yeonwan <yeonwan@naver.com>
2019-12-03 20:52:20 +0900

    delete edit

--- src/main/java/com/foodmanager/server/services/FoodHandleService.java
+++ src/main/java/com/foodmanager/server/services/FoodHandleService.java
@@ -52,7 +52,7 @@ public class FoodHandleService {
     }
 
     private int findFoodId(String fName){
-        String sql = String.format("SELECT Name FROM Food WHERE ID = \"%s\"", fName);
+        String sql = String.format("SELECT Name FROM Food WHERE Name = \"%s\"", fName);
         return dbRepository.queryForObject(sql, Integer.class);
     }
 
